52537ae52bfe18adf1f74b845a88ca20ae3c0eb3
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-10-12 20:06:58 -0300

    Create nameUser.cpp

---
+++ C++/Object Oriented/nameUserget/nameUser.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+class nameuser{//Name class
+
+    //Variables
+    string Name;
+    string Nickname;
+    list<string> publicname;
+
+
+};
+
+  //Variables and variables
+nameuser(string Name, string Nickname)
+{
+
+    Name=name;
+    Nickname=nickname;
+
+
+}
+
+//Print about information name and nickname
+
+  void GetInfor()
+{
+    cout << "Name: " << Name << endl;
+    cout << "Nickname: " << Nickname << endl;
+
+}
+
+
+int main()
+{
+
+    //Information about name,nickname and age people
+    nameuser.nameUser;
+    nameUser.Name=("Tassiana Milka");
+    nameUser.Nickname =("Milka,Tassi or Tata");
+
+    //Return about information
+
+    nameUser.GetInfor();
+
+
+
+   system("pause>0");
+
+
